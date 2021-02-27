15b89fac639fdc9db2f0f7e176964725f8939ec0
Sowon Jung <swj960515@gmail.com>
2021-02-27 18:44:55 +0900

    update/client: 네트워크 페이지에서 포트폴리오 페이지로 연결되는 기능, 포트폴리오 페이지에 프로필 불러오기 기능

--- client/src/components/network/Profile.js
+++ client/src/components/network/Profile.js
@@ -5,9 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 // mode: view(network 전용), read(portfolio 페이지 전용), update(portfolio 페이지 수정 전용)
-function Profile({ mode, imgurl, name, comment }) {
+function Profile({ id, mode, imgurl, name, comment, onClick }) {
   const default_img_url =
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+  const buttons = [
+    (<Button key={0} variant="primary" id={id} onClick={onClick}>정보 보기</Button>),
+    (<Button key={1} variant="primary"><FontAwesomeIcon icon={faPen} /></Button>),
+    (<Button key={2} variant="light"><FontAwesomeIcon icon={faCheck} /></Button>)
+  ]
   const imgComponent = (
     <Card.Img
       src={imgurl && default_img_url}
@@ -24,42 +29,36 @@ function Profile({ mode, imgurl, name, comment }) {
           padding: '20px 20px',
         }}
       >
-        {mode !== 'update' ? imgComponent : <a>{imgComponent}</a>}
+        {mode + '' !== 'update' ? imgComponent : <a>{imgComponent}</a>}
       </Card.Header>
       <Card.Body style={{ textAlign: 'center' }}>
         <Card.Title>
-          {mode !== 'update' ? (
+          {mode + '' !== 'update' ? (
             name || 'Noname'
           ) : (
               <Form.Control type="text" placeholder="이름" />
             )}
         </Card.Title>
         <Card.Text>
-          {mode !== 'update' ? (
+          {mode + '' !== 'update' ? (
             !comment && '한 줄 소개'
           ) : (
               <Form.Control type="text" placeholder="한 줄 소개" />
             )}
         </Card.Text>
-        {(mode !== 'update') ? (
-          <Button variant="primary">
-            {mode === 'view' ? '정보 보기' : <FontAwesomeIcon icon={faPen} />}
-          </Button>
-        ) : (
-            <Button variant="light">
-              <FontAwesomeIcon icon={faCheck} />
-            </Button>
-          )}
+        {buttons[mode]}
       </Card.Body>
     </Card>
   );
 }
 
 Profile.propTypes = {
-  mode: PropTypes.string,
+  id: PropTypes.number,
+  mode: PropTypes.number,
   imgurl: PropTypes.string,
   name: PropTypes.string,
   comment: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default Profile;
